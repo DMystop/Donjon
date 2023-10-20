6650c02c7fd29d0d28c424fc3838b21054e96d97
Kylariria <148540560+Kylariria@users.noreply.github.com>
2023-10-20 14:18:49 +0200

    Update Main.cpp
    Variables

--- DonjonDeLaMuerte/DonjonDeLaMuerte/Main.cpp
+++ DonjonDeLaMuerte/DonjonDeLaMuerte/Main.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 void main()
 {
-	
+	int _maxPlayerLife = 100;
+	int _actualPlayerLife = 100;
+	int _monsterPv = 100;
+	int _scytheAttack = 30;
+	int _gunAttack = 10;
+	bool _playerAnswer;
 }
 
 void Attack()
