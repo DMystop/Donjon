977ff23984d0533551caeee1d4ca5fba1620af8d
Kylariria <iriapaquis.pro@gmail.com>
2023-10-20 15:31:43 +0200

    Update Main.cpp
    update code

--- DonjonDeLaMuerte/DonjonDeLaMuerte/Main.cpp
+++ DonjonDeLaMuerte/DonjonDeLaMuerte/Main.cpp
@@ -7,25 +7,35 @@ using namespace std;
 void Introduction();
 void PNJ();
 void Event();
-void Attack();
-void Healing();
+void PlayerAttackMonster(int _weaponUsed, int _monsterPv);
+void MonsterAttackPlayer(int _actualPlayerLife, int _monsterAttack);
+void Healing(int _actualPlayerLife, int _healPotion, int _healPotionPower, int _maxPlayerLife);
 void ActualWeapon();
 void SwapWeapon();
 void FightMovement();
 void Movement();
-void Display(string _storyText)
+void Display(string _storyText);
 
 #pragma endregion
 
-void main()
+int main()
 {
+	int _healPotion = 3;
+	int _healPotionPower = 20;
 	int _maxPlayerLife = 100;
 	int _actualPlayerLife = 100;
 	int _monsterPv = 100;
 	int _scytheAttack = 30;
 	int _gunAttack = 10;
-	bool _playerAnswer;
+	int _monsterAttack = 20;
+	bool _playerAnswer=0;
 	string _storyText;
+
+	Introduction();
+	MonsterAttackPlayer(_actualPlayerLife, _monsterAttack);
+	Healing(_actualPlayerLife, _healPotion, _healPotionPower, _maxPlayerLife); 
+
+	return 0;
 }
 
 #pragma region Texts
@@ -34,8 +44,8 @@ void Introduction()
 {
 	Display("Bienvenue dans le Donjon de la Muerte !");
 	Display("Vous venez de spawn dans ce donjon suite au choix de votre Guilde, la Guilde Hestia.");
-	Display("On vous a donné deux armes : la Scythe (corps à corps et arme principale).");
-	Display("Vous êtes l’aventurier le plus faible de la guilde donc pour level up, on vous a donné cette mission.");
+	Display("On vous a donn\x82\ deux armes : la Scythe (corps \x85\ corps et arme principale).");
+	Display("Vous \x88tes l’aventurier le plus faible de la guilde donc pour level up, on vous a donn\x8A\ cette mission.");
 	Display("Votre mission est de battre le Dragon de Foudre à la fin de ce donjon.");
 	Display("Ce donjon est rempli de mystères et de surprises. A vous de les découvrir.");
 	Display("Bonne chance à vous jeune aventurier.");
@@ -57,34 +67,62 @@ void Event()
 }
 
 #pragma endregion
-void Attack()
+
+void PlayerAttackMonster(int _weaponUsed, int _monsterPv)
 {
-	
+	_monsterPv -= _weaponUsed;
+	cout << "Le joueur attaque " << _weaponUsed << " pv ! " << endl;
 }
 
-void Healing()
+void MonsterAttackPlayer(int _actualPlayerLife, int _monsterAttack)
 {
-	
+	_actualPlayerLife -= _monsterAttack;
+	cout << "Le joueur perd " << _monsterAttack << " pv ! " << endl << "Il lui reste " << _actualPlayerLife << " pv !" << endl;
+}
+
+void Healing(int _actualPlayerLife, int _healPotion, int _healPotionPower, int _maxPlayerLife)
+{
+	cout << "Tu as choisi de te heal." << endl;
+
+	if (_healPotion == 0)
+	{
+		Display("Vous ne posseder plus de potions. Bonne chance ;)");
+		return;
+	}
+
+	if (_actualPlayerLife + _healPotionPower >= 100)
+	{
+		_actualPlayerLife = 100;
+	}
+
+	else
+	{
+		_actualPlayerLife += _healPotionPower;
+
+	}
+
+	cout << "Votre vie est de " << _actualPlayerLife << endl;
+
 }
 
 void ActualWeapon()
 {
-	
+
 }
 
 void SwapWeapon()
 {
-	
+
 }
 
 void FightMovement()
 {
-	
+
 }
 
 void Movement()
 {
-	
+
 }
 
 void Display(string _storyText)
