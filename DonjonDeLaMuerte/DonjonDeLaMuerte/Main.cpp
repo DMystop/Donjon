3d4f3fe364af82b1f10013444084b967f80b2053
Kylariria <iriapaquis.pro@gmail.com>
2023-10-20 14:55:06 +0200

    Update Main.cpp
    Dialogue intro, pnj, event

--- DonjonDeLaMuerte/DonjonDeLaMuerte/Main.cpp
+++ DonjonDeLaMuerte/DonjonDeLaMuerte/Main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 #pragma region Forwards
 
+void Introduction();
+void PNJ();
+void Event();
 void Attack();
 void Healing();
 void ActualWeapon();
@@ -25,12 +28,35 @@ void main()
 	string _storyText;
 }
 
+#pragma region Texts
+
 void Introduction()
 {
 	Display("Bienvenue dans le Donjon de la Muerte !");
-	Display("")
+	Display("Vous venez de spawn dans ce donjon suite au choix de votre Guilde, la Guilde Hestia.");
+	Display("On vous a donné deux armes : la Scythe (corps à corps et arme principale).");
+	Display("Vous êtes l’aventurier le plus faible de la guilde donc pour level up, on vous a donné cette mission.");
+	Display("Votre mission est de battre le Dragon de Foudre à la fin de ce donjon.");
+	Display("Ce donjon est rempli de mystères et de surprises. A vous de les découvrir.");
+	Display("Bonne chance à vous jeune aventurier.");
 }
 
+void PNJ()
+{
+	Display("Bonjour à vous très cher. Que faites-vous là tout seul ?");
+	Display("C’est très dangereux dans le coin. Es-tu là pour le dragon ?");
+	Display("Je te conseille de tourner à droite pour le rencontrer.");
+	Display("Au revoir mon p’tit bonhomme.");
+}
+
+void Event()
+{
+	Display("Vous tombez nez à nez face à un coffre.");
+	Display("Vous l’ouvrez et trouvez un Gun (arme à distance et placé en arme secondaire).");
+	Display("Félicitations, vous avez obtenu un Gun !");
+}
+
+#pragma endregion
 void Attack()
 {
 	
